b49f6d7106e36825407dd4f9afc5d430a3d31c03
thetiagogil <talmeidagil@gmail.com>
2026-05-19 00:47:25 +0100

    refactor: enhance ScrollToTop functionality and remove smooth scroll behavior

--- src/shared/components/common/ScrollToTop.tsx
+++ src/shared/components/common/ScrollToTop.tsx
@@ -1,12 +1,12 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export function ScrollToTop() {
+export const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+    window.scrollTo(0, 0);
   }, [pathname]);
 
   return null;
-}
+};
